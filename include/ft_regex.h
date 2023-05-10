53c4aab0bb73752a0341a449cfcb444eb87e3a86
ltuffery <ltuffery@student.42.fr>
2023-05-10 18:51:53 +0200

    add option flags

--- include/ft_regex.h
+++ include/ft_regex.h
@@ -1,6 +1,13 @@
 #ifndef FT_REGEX
 # define FT_REGEX
 
+# define O_GLOBAL 1
+# define O_MULTILINE 2
+# define O_INSENSITIVE 4
+# define O_EXTENDED 8
+# define O_SINGLELINE 16
+# define O_UNICODE 32
+
 typedef struct	s_matches {
 	char 				*content;
 	struct s_matches	*next;
